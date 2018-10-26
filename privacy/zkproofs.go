1027a1b358da9c3e95954c980863e195c7e946bc
0xkraken <hien.n@autonomous.nyc>
2018-10-26 15:29:32 +0700

    update name proof

--- privacy/zkproofs.go
+++ privacy/zkproofs.go
@@ -12,25 +12,18 @@ package privacy
 
 // }
 
-//ZkpPedersenCM contains proof's value
-type ZkpPedersenCM struct {
-<<<<<<< HEAD
+//ZkpPedersenCMProof contains proof's value
+type ZkpPedersenCMProof struct {
 	Alpha, Beta, GammaAddr, GammaValue, GammaSN, GammaR []byte
-=======
-	Gamma1, Gamma2, Gamma3, Gamma4 []byte
->>>>>>> aa3494236443b7891d5c2f5681f3035e08c77de1
 }
 
 //ZkpPedersenCMProve create zero knowledge proof for an opening of a Pedersen commitment
-func ZkpPedersenCMProve(cm CommitmentParams, pubKey PublicKey, sn SerialNumber, value []byte) *ZkpPedersenCM {
-	zkp := new(ZkpPedersenCM)
+func ZkpPedersenCMProve(cm CommitmentParams, pubKey PublicKey, sn SerialNumber, value []byte) *ZkpPedersenCMProof {
+	zkp := new(ZkpPedersenCMProof)
 	return zkp
 }
 
-<<<<<<< HEAD
 //ZkpPedersenCMVerify check the proof's value
-func ZkpPedersenCMVerify(proofsvalue ZkpPedersenCM, commintmentsvalue []byte) bool {
+func ZkpPedersenCMVerify(proofsvalue ZkpPedersenCMProof, commintmentsvalue []byte) bool {
 	return true
 }
-=======
->>>>>>> aa3494236443b7891d5c2f5681f3035e08c77de1
