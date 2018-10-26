74c5b7a5b2a90b0fae51472f25c76185ba83e1e2
0xthunderbird <dat@ninja.org>
2018-10-26 15:17:18 +0700

    compute proof

--- privacy/zkproofs.go
+++ privacy/zkproofs.go
@@ -1,5 +1,10 @@
 package privacy
 
+import (
+	"math/rand"
+	"time"
+)
+
 // type Proof interface{
 // 	Params()
 
@@ -13,11 +18,25 @@ package privacy
 // }
 
 type ZkpPedersenCM struct {
-	Gamma1, Gamma2 []byte
+	Gamma1, Gamma2, Gamma3, Gamma4 []byte
 }
-
+func getRandom() []byte{
+	rand.Seed(time.Now().UTC().Unix())
+	r:= make([]byte,32)
+	rand.Read(r)
+	return r
+}
 // ZkpPedersenCMComponent create zero knowledge proof for an opening of a Pedersen commitment
-func ZkpPedersenCMComponent(cm CommitmentParams, pubKey PublicKey, sn SerialNumber, value []byte) *ZkpPedersenCM{
+func pedersenCMGenerateProof(cm CommitmentParams, pubKey PublicKey, sn SerialNumber, value []byte) *ZkpPedersenCM{
 	zkp := new(ZkpPedersenCM)
+	rand.Seed(time.Now().UTC().Unix())
+	r0:=getRandom();
+	r1:=getRandom();
+	r2:=getRandom();
+	r3:=getRandom();
+	alpha:=new(EllipticPoint)
+
+	alpha.X, alpha.Y = Curve.ScalarMult(cm.G0.X,cm.G0.Y, r0);
+	alpha.X, alpha.Y = Curve.Add(alpha.X, alpha.Y, Curve.ScalarMult(cm.G1.X, cm.G1.Y,r1))
 	return zkp
 }
